ed1aebe6f03eeb4fadc6ebd437e856a7d9bfed1b
Eukkbi <smbaekr56@naver.com>
2021-12-22 19:35:08 +0900

    닷트윈 추가 , 타이틀 이동

---
+++ Assets/Scripts/Title/Touch.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Touch : MonoBehaviour
+{
+    public ParticleSystem normalParticle;
+    public ParticleSystem changeParticle;
+    public CanvasGroup titleCanvasGroup;
+
+
+    public void ChangeTitleStart()
+    {
+        StartCoroutine(ChangeTitle());
+    }
+
+    public IEnumerator ChangeTitle()
+    {
+        ParticleSystem.MainModule normal = normalParticle.main;
+        ParticleSystem.MainModule change = changeParticle.main;
+
+        normal.startLifetime = 0.00001f;
+
+        changeParticle.gameObject.SetActive(true);
+
+        yield return new WaitForSeconds(1f);
+
+        titleCanvasGroup.DOFade(0,.5f).OnComplete(() =>
+        {
+            change.startLifetime = 0.00001f;
+            normalParticle.gameObject.SetActive(false);
+            titleCanvasGroup.gameObject.SetActive(false);
+        });
+        yield return new WaitForSeconds(3f);
+        changeParticle.gameObject.SetActive(false);
+    }
+}
