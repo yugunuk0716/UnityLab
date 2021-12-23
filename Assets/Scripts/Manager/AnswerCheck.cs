d87beb7df79cd8ca9ad22525f34b433ce61fe144
DESKTOP-MG9TML9\USER <qheorjsdnr@naver.com>
2021-12-23 18:11:12 +0900

    파일 이름 변경

--- Assets/Scripts/Manager/AnswerCheck.cs
+++ Assets/Scripts/Manager/AnswerCheck.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class AnswerManager : Singleton<AnswerManager>
+public class AnswerCheck : Singleton<AnswerCheck>
 {
     public bool isPremium = false;
 
