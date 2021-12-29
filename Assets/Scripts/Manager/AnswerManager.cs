4014130cb4b8c7355df72d0a6b9a7bc07a408763
3soojung <68137693+kungsae@users.noreply.github.com>
2021-12-29 17:57:01 +0900

    텍스트 프리팹 생성

---
+++ Assets/Scripts/Manager/AnswerManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerManager : MonoBehaviour
+{
+	[SerializeField] private Transform content;
+	[SerializeField] private GameObject textPrefab;
+
+	private void Start()
+	{
+
+	}
+	public GameObject OutPutText(string _str,string _answer)
+	{
+		GameObject text = Instantiate(textPrefab,content);
+		TextArea data = text.GetComponent<TextArea>();
+		Text strText = text.transform.Find("text").GetComponent<Text>();
+		Text lineIndex = text.transform.Find("LineIndex").GetComponent<Text>();
+		strText.text = _str;
+
+		if (data != null)
+		{
+			data.Answer = _answer;
+			lineIndex.text = data.index.ToString();
+		}
+		return text;
+	}
+}
