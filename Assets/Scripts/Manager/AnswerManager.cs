c72ae5a7d46aa646fb60a51be9915ec9e2184c65
3soojung <68137693+kungsae@users.noreply.github.com>
2021-12-30 23:29:10 +0900

    시발 수정

--- Assets/Scripts/Manager/AnswerManager.cs
+++ Assets/Scripts/Manager/AnswerManager.cs
@@ -11,30 +11,7 @@ public class AnswerManager : Singleton<AnswerManager>
 	[SerializeField] private Transform content;
 	[SerializeField] private GameObject textPrefab;
 
-	//public TextMeshProUGUI test;
-	private void Start()
-	{
-		/*print(test.text.Length);
-		print(test.GetParsedText().Length);
-		print(test.GetParsedText());*/
-		/*string[] strs = test.text.Split(new string[] { "<blink>", "</blink>" }, StringSplitOptions.None);
-		int idx = 0;
-*/
-		/*for (int i = 0; i < strs.Length; i++)
-		{
 
-			if (i % 2 != 0)
-			{
-				//박스 생성 만들면됨(strs[i]);
-				//strs[i] = "                                ";
-				strs[i] = " ";
-			}
-			idx = test.GetParsedText().Length;
-			string a = test.GetParsedText();
-			print(a);
-			print(idx);
-		}*/
-	}
 	public GameObject OutPutText(string _str,int lineIdx)
 	{
 		GameObject text = Instantiate(textPrefab,content);
@@ -55,14 +32,8 @@ public class AnswerManager : Singleton<AnswerManager>
 					//strs[i] = "                                ";
 					strs[i] = " ";
 				}
-				strText.text += strs[i];
-				idx = strText.GetParsedText().Length;
-				string a = strText.GetParsedText();
 
-/*
-				Debug.Log(strText.text);
-				Debug.Log(strText.GetParsedText());
-				print(a);*/
+				StartCoroutine(ParseText(strText, strs, i));
 			}
 			lineIndex.text = lineIdx.ToString();
 			return text;
@@ -72,5 +43,18 @@ public class AnswerManager : Singleton<AnswerManager>
 		lineIndex.text = lineIdx.ToString();
 
 		return text;
+	}
+
+
+	public IEnumerator ParseText(TextMeshProUGUI StrText, string[] strs, int i)
+	{
+		int idx = 0;
+		yield return null;
+		StrText.text += strs[i];
+		idx = StrText.GetParsedText().Length;
+		string a = StrText.GetParsedText();
+
+
+		Debug.Log(StrText.GetParsedText().Length);
 	}
 }
