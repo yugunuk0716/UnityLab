81f9084fcc408e1ccc56aa5b9a99303c5705a906
juhyeong0823 <juhyeong0823@gmail.com>
2021-12-31 10:43:15 +0900

    place answerArea Second try

--- Assets/Scripts/GetTextPosition.cs
+++ Assets/Scripts/GetTextPosition.cs
@@ -5,33 +5,33 @@ using TMPro;
 public class GetTextPosition : MonoBehaviour
 {
     //테스트 스크립트입니다.
-    //public TextMeshProUGUI tmp_text;
+    public TextMeshProUGUI tmp_text;
     public GameObject testobj;
-    public Transform p;
+    public GameObject answerArea;
+    public Transform parent;
     public string a = "<blink><color=#DCDCDC>asd</color></blink>aaaccc";
+    public int o;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.P))
         {
-            TextMeshProUGUI tmp_text = GameObject.Find("text").GetComponent<TextMeshProUGUI>();
             tmp_text.text = a;
             tmp_text.text = tmp_text.GetParsedText().Replace("<blink>", string.Empty).Replace("</blink>", string.Empty);
             Debug.Log(tmp_text.text.Length);
+            MakeAnswerArea(tmp_text, o);
         }
     }
 
-    /*
-    public void MakeAnswerArea(TextMeshProUGUI tmp_text)
+
+    public void MakeAnswerArea(TextMeshProUGUI tmp_text, int index)
     {
         tmp_text.ForceMeshUpdate();
         Vector3[] vertices = tmp_text.mesh.vertices;
-        TMP_CharacterInfo charInfo = tmp_text.textInfo.characterInfo[0];
+        TMP_CharacterInfo charInfo = tmp_text.textInfo.characterInfo[o];
         int vertexIndex = charInfo.vertexIndex;
         Vector2 charMidTopLine = new Vector2((vertices[vertexIndex + 0].x + vertices[vertexIndex + 2].x) / 2, (charInfo.bottomLeft.y + charInfo.topLeft.y) / 2);
         Vector3 worldPos = tmp_text.transform.TransformPoint(charMidTopLine);
-        GameObject charPositionGameObj = Instantiate(answerArea, parentTrm);
+        GameObject charPositionGameObj = Instantiate(answerArea, parent);
         charPositionGameObj.transform.position = worldPos;
     }
-    */
-
 }
--- Assets/Scripts/Manager/AnswerManager.cs
+++ Assets/Scripts/Manager/AnswerManager.cs
@@ -8,72 +8,75 @@ using System.Text.RegularExpressions;
 
 public class AnswerManager : Singleton<AnswerManager>
 {
-	[SerializeField] private Transform content;
-	[SerializeField] private GameObject textPrefab;
+    [SerializeField] private Transform content;
+    [SerializeField] private GameObject textPrefab;
 
-	public GameObject answerArea;
-	public Transform parentTrm;
-
-	private void Start()
-	{
-
-	}
-	public GameObject OutPutText(string _str,int lineIdx)
-	{
-		GameObject text = Instantiate(textPrefab,content);
-		//TextArea data = text.GetComponent<TextArea>();
-		TextMeshProUGUI strText = text.transform.Find("text").GetComponent<TextMeshProUGUI>();
-		Text lineIndex = text.transform.Find("LineIndex").GetComponent<Text>();
-		//strText.ForceMeshUpdate();
-
-		if (_str.IndexOf("<blink>")!=-1)
-		{
-			string[] strs = _str.Split(new string[] { "<blink>", "</blink>" }, StringSplitOptions.None);
-			StartCoroutine(ParseText(strText, strs,text));
-			
-			lineIndex.text = lineIdx.ToString();
-			return text;
-		}
-
-		strText.text = _str;
-		lineIndex.text = lineIdx.ToString();
-
-		return text;
-	}
+    public GameObject answerArea;
+    public Transform parentTrm;
 
 
-	public IEnumerator ParseText(TextMeshProUGUI StrText, string[] strs, GameObject text)
-	{
-		int idx = 0;
-		StrText.text = "";
-		for (int i = 0; i < strs.Length; i++)
-		{
-			if (i % 2 != 0)
-			{
-				CreateAnswerArea(text, idx);
-				strs[i] = "                                ";
-				//strs[i] = "      ";
-			}
-			StrText.text += strs[i];
-			yield return null;
-			idx = StrText.GetParsedText().Length;
-		}
-	}
-	public void CreateAnswerArea(GameObject text, int index)
-	{
-	 	GameObject answer= Instantiate(answerArea, text.transform);
-		RectTransform anwserTrm = answer.GetComponent<RectTransform>();
+    public GameObject OutPutText(string _str, int lineIdx)
+    {
+        GameObject text = Instantiate(textPrefab, content);
+        //TextArea data = text.GetComponent<TextArea>();
+        TextMeshProUGUI strText = text.transform.Find("text").GetComponent<TextMeshProUGUI>();
+        Text lineIndex = text.transform.Find("LineIndex").GetComponent<Text>();
+        //strText.ForceMeshUpdate();
+
+        if (_str.IndexOf("<blink>") != -1)
+        {
+            string[] strs = _str.Split(new string[] { "<blink>", "</blink>" }, StringSplitOptions.None);
+            StartCoroutine(ParseText(strText, strs, text));
+
+            lineIndex.text = lineIdx.ToString();
+            return text;
+        }
+
+        strText.text = _str;
+        lineIndex.text = lineIdx.ToString();
+
+        return text;
+    }
 
 
-	}
-	public void MakeAnswerArea(TextMeshProUGUI tmp_text,int index)
-	{
-		Debug.Log(index);
-		Vector3[] vertices = tmp_text.mesh.vertices;
-		TMP_CharacterInfo charInfo = tmp_text.textInfo.characterInfo[index];
-		int vertexIndex = charInfo.vertexIndex;
-		Vector2 charMidTopLine = new Vector2((vertices[vertexIndex + 0].x + vertices[vertexIndex + 2].x) / 2, (charInfo.bottomLeft.y + charInfo.topLeft.y) / 2);
-		Vector3 worldPos = tmp_text.transform.TransformPoint(charMidTopLine);
-		Instantiate(answerArea, worldPos, Quaternion.identity, parentTrm);
-	}
+    public IEnumerator ParseText(TextMeshProUGUI StrText, string[] strs, GameObject text)
+    {
+        int idx = 0;
+        StrText.text = "";
+        for (int i = 0; i < strs.Length; i++)
+        {
+            if (i % 2 != 0)
+            {
+                MakeAnswerArea(StrText, idx, text.transform);
+                strs[i] = "                                "; // 공백32개
+                Debug.Log("텍스트 : " + StrText.text);
+                Debug.Log("문자열 길이 : " + StrText.text.Length);
+                Debug.Log("인덱스 : " + idx);
+                Debug.Log("인덱스 해당 텍스트 : " + StrText.text[idx]);
+                Debug.Log("인덱스-1 해당 텍스트 : " + StrText.text[idx - 1]);
+                Debug.Log("인덱스+1 해당 텍스트 : " + StrText.text[idx + 1]);
+            }
+            StrText.text += strs[i];
+            yield return null;
+            idx = StrText.GetParsedText().Length;
+        }
+    }
+
+
+
+    public void MakeAnswerArea(TextMeshProUGUI tmp_text, int index, Transform parent)
+    {
+        Debug.Log(index);
+        tmp_text.ForceMeshUpdate();
+        Vector3[] vertices = tmp_text.mesh.vertices;
+        TMP_CharacterInfo charInfo = tmp_text.textInfo.characterInfo[index - 1];
+        Debug.Log(tmp_text.textInfo.characterInfo[index - 1].character.ToString());
+        int vertexIndex = charInfo.vertexIndex;
+        Debug.Log(vertices.Length);
+
+        Vector2 charMidTopLine = new Vector2((vertices[index - 1].x) / 2, (charInfo.bottomLeft.y + charInfo.topLeft.y) / 2);
+        Vector3 worldPos = tmp_text.transform.TransformPoint(charMidTopLine);
+        GameObject charPositionGameObj = Instantiate(answerArea, parent);
+        charPositionGameObj.transform.position = worldPos;
+    }
 }
