87e3edc2035b9fbea2283d679ac9134e6af82e79
juhyeong0823 <juhyeong0823@gmail.com>
2022-01-03 23:26:40 +0900

    인게임 화면 크기 조정?      (정의재를 죽여주세요)

--- Assets/Scripts/Manager/AnswerManager.cs
+++ Assets/Scripts/Manager/AnswerManager.cs
@@ -14,18 +14,19 @@ public class AnswerManager : Singleton<AnswerManager>
     public GameObject answerArea;
     public Transform parentTrm;
 
+    float answerAreaWidth;
+
     void Start()
     {
-        //OutPutText("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa<blink>aa</blink>aaaa", 55);
+        answerAreaWidth = answerArea.GetComponent<RectTransform>().rect.width;
     }
+
 	public GameObject OutPutText(string _str, int lineIdx,GameObject content)
     {
         GameObject text = Instantiate(textPrefab, content.transform);
-        //TextArea data = text.GetComponent<TextArea>();
         TextMeshProUGUI strText = text.transform.Find("text").GetComponent<TextMeshProUGUI>();
         Text lineIndex = text.transform.Find("LineIndex").GetComponent<Text>();
         lineIndex.text = "";
-        //strText.ForceMeshUpdate();
 
         if (_str.IndexOf("<blink>") != -1)
         {
@@ -41,6 +42,7 @@ public class AnswerManager : Singleton<AnswerManager>
         strText.text = _str;
         if (lineIdx != 0)
             lineIndex.text = lineIdx.ToString();
+
         StartCoroutine(wait(strText, content));
         return text;
     }
@@ -49,8 +51,7 @@ public class AnswerManager : Singleton<AnswerManager>
     {
         if (content.GetComponent<VerticalLayoutGroup>().padding.right < strText.bounds.size.x)
         {
-            print(strText.rectTransform.rect.width);
-            content.GetComponent<VerticalLayoutGroup>().padding.right = (int)strText.rectTransform.rect.width + 50;
+            content.GetComponent<VerticalLayoutGroup>().padding.right = (int)(strText.bounds.size.x + answerAreaWidth * 2.5f); // 3개이상 안드감
         }
     }
     public IEnumerator wait(TextMeshProUGUI strText, GameObject content)
@@ -61,36 +62,16 @@ public class AnswerManager : Singleton<AnswerManager>
 
     public IEnumerator ParseText(TextMeshProUGUI StrText, string[] strs, GameObject text)
     {
-        int idx = 0;
         StrText.text = "";
-       // TextMeshProUGUI fake = Instantiate(fakeTxt, StrText.transform.parent).GetComponent<TextMeshProUGUI>();
-        
-        //fake.text = "";
         for (int i = 0; i < strs.Length; i++)
         {
             if (i % 2 != 0)
             {
-                //MakeAnswerArea(fake, idx, text.transform);
                 testMake(StrText);
-                strs[i] = "                 "; // 공백17개
-                //print("A"+idx);
-                //print("B"+fake.text.Length);
-				//Debug.Log("텍스트 : " + fake.text);
-				//Debug.Log("문자열 길이 : " + fake.text.Length);
-				//Debug.Log("인덱스 : " + idx);
-				//Debug.Log("인덱스 해당 텍스트 : " + fake.text[idx]);
-				//Debug.Log("인덱스-1 해당 텍스트 : " + fake.text[idx - 1]);
-				//Debug.Log("인덱스+1 해당 텍스트 : " + fake.text[idx + 1]);
+                strs[i] = "                 "; 
 			}
-
-            //string test = strs[i].Replace(' ', 'a');
-            //fake.text += test;
-            //yield return null;
-            //fake.text = fake.GetParsedText();
             StrText.text += strs[i];
-
             yield return null;
-            idx = StrText.GetParsedText().Length;
         }
     }
 
@@ -101,19 +82,5 @@ public class AnswerManager : Singleton<AnswerManager>
         print(tmp_text.bounds.size.x);
         area.GetComponent<RectTransform>().anchoredPosition = new Vector3(100+tmp_text.bounds.size.x,0);
     }
-    //public void MakeAnswerArea(TextMeshProUGUI tmp_text, int index, Transform parent)
-    //{
-    //    Debug.Log("index : " + index);
-    //    tmp_text.ForceMeshUpdate();
-    //    Vector3[] vertices = tmp_text.mesh.vertices;
-    //    TMP_CharacterInfo charInfo = tmp_text.textInfo.characterInfo[index - 1];
-    //    int vertexIndex = charInfo.vertexIndex;
-    //    Debug.Log("vertices : " + vertices.Length);
-    //    Debug.Log("vertexIndex : " + vertexIndex);
 
-    //    Vector2 charMidTopLine = new Vector2((vertices[vertexIndex / 2 - 1].x + vertices[vertexIndex / 2 - 2].x) / 2, (charInfo.bottomLeft.y + charInfo.topLeft.y) / 2);
-    //    Vector3 worldPos = tmp_text.transform.TransformPoint(charMidTopLine);
-    //    GameObject charPositionGameObj = Instantiate(answerArea, parent);
-    //    charPositionGameObj.transform.position = worldPos;
-    //}
 }
--- Assets/Scripts/Manager/CreateAnswer.cs
+++ Assets/Scripts/Manager/CreateAnswer.cs
@@ -14,42 +14,34 @@ public class CreateAnswer : MonoBehaviour
     [SerializeField] private GameObject buttonPrefab;
 
     private List<GameObject> scriptsArray = new List<GameObject>();
-    // Start is called before the first frame update
     void Start()
     {
-		//print(ReadTxt(1));
-		//ReadTxt(1);
 		for (int i = 0; i < StageManager.instance.stageInScriptsCount[StageManager.instance.stageIdx]; i++)
 		{
             CreateBase(i + 1);
         }
-
     }
 
     private void CreateBase(int i)
     {
         GameObject buttonObj = Instantiate(buttonPrefab, buttonParent.transform);
-
         GameObject baseObj = Instantiate(basePrefab, parent.transform);
-        
         GameObject baseParent = baseObj.transform.Find("Scroll View").gameObject.transform.Find("Viewport").gameObject.transform.Find("Content").gameObject;
-
 
         scriptsArray.Add(baseObj);
         buttonObj.GetComponent<Button>().onClick.AddListener(() =>
         {
-			    for (int i = 0; i < scriptsArray.Count; i++)
-			    {
-                    scriptsArray[i].SetActive(false);
-                }
-                baseObj.SetActive(true);
+		    for (int i = 0; i < scriptsArray.Count; i++)
+		    {
+                scriptsArray[i].SetActive(false);
             }
-        );
+            baseObj.SetActive(true);
+        });
 
         int idx = StageManager.instance.stageIdx;
         ReadTxt($"{idx}-{StageManager.instance.stageInScriptsCount[i]}", baseParent);
     }
-    // Update is called once per frame
+
     private string ReadTxt(string stageIdx,GameObject parent)
     {
         string filePath = Application.dataPath + "/Resources/Question " + stageIdx + ".txt";
@@ -73,18 +65,19 @@ public class CreateAnswer : MonoBehaviour
         {
             str = str.Replace("<3>", "      ");
         }
+        if (str.Contains("<4>"))
+        {
+            str = str.Replace("<4>", "        ");
+        }
         //\W<>=#*
 
-        //string[] strs = str.Split(new string[] { "^","<blink>","</blink>" }, StringSplitOptions.None);
         string[] strs = str.Split('^');
         int idx = 0;
         foreach (var item in strs)
         {
             idx++;
-            //print(item);
             AnswerManager.Instance.OutPutText(item,idx, parent);
         }
         return str;
     }
 }
-//gayjoygo
