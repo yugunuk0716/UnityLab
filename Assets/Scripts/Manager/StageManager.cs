b6f5de007288b361293ce751f24662d0f8c306ab
hwanjuson <hwanjuson@gmail.com>
2022-01-03 18:12:09 +0900

    스테이지 인덱스 설정

--- Assets/Scripts/Manager/StageManager.cs
+++ Assets/Scripts/Manager/StageManager.cs
@@ -7,15 +7,23 @@ using UnityEngine.UI;
 public class StageManager : Singleton<StageManager>
 {
     public int stageIdx = 1;
+    public Dictionary<int, int> stageInIdx;
+    public int[] stageInScriptsCount;
     private int buttonCount;
 
     public Transform btnParent; //버튼 부모
     public Button[] stageBtns { get; private set; } //인게임 이동 버튼
 
+
     private void Start()
     {
+        stageInScriptsCount = new int[]
+        {
+            -1,2,3,2,1,2,2,1
+        };
         #region 버튼 관련
         stageBtns = btnParent.GetComponentsInChildren<Button>();
+        stageInIdx = new Dictionary<int, int>();
         buttonCount = stageBtns.Length;
 
         for (int i = 0; i < buttonCount; i++)
@@ -23,7 +31,8 @@ public class StageManager : Singleton<StageManager>
             int idx = i;
             stageBtns[idx].onClick.AddListener(() =>
             {
-                stageIdx = i + 1;
+                stageIdx = idx + 1;
+                stageInIdx.Add(idx, stageInScriptsCount[idx - 1]);
                 SceneManager.LoadScene("InGame");
             });
         }   
