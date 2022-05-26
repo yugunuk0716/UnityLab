3c83f437c6def10c5bedecb2fc9d3431155e3111
DESKTOP-MG9TML9\USER <qheorjsdnr@naver.com>
2022-05-26 11:19:00 +0900

    타이틀 화면전환 파티클 수정, 공백 제거, 인게임 UI 다시 만들기 등의 UI 작업과 UI 관련코드 수정

--- Assets/Scripts/Manager/CreateAnswer.cs
+++ Assets/Scripts/Manager/CreateAnswer.cs
@@ -38,7 +38,7 @@ public class CreateAnswer : MonoBehaviour
         TextMeshProUGUI t = buttonObj.GetComponentInChildren<TextMeshProUGUI>();
         t.text = strs[1];
         yield return null;
-        t.text = t.GetParsedText() + ".cs";
+        t.text = $"{t.GetParsedText().Substring(0, t.GetParsedText().Length - 1)}.cs";
     }
 
     private void CreateBase(int i)
